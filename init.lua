3bd05c295fd7e84c6f080ac3d7294179110b23e6
Sijmen Woutersen <sijmen.woutersen@gmail.com>
2024-09-14 13:24:40 +0200

    initial

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("config.lazy")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.opt.mouse = ""
+vim.opt.number = true
+vim.opt.relativenumber = true
+
