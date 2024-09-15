6d5ad2ba2d095d054444e301300edb2da87983a5
Sijmen Woutersen <sijmen.woutersen@gmail.com>
2024-09-15 10:32:08 +0200

    add nvim-tree

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,13 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.o.mouse = ""
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.o.termguicolors = true
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
